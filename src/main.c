708b3101721a98ec326b9cb9c1b29b4dbf885f21
pw <przemyslawxdab@gmail.com>
2026-01-27 21:52:32 +0100

    detect and print digipeating instructions

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include <signal.h>
 #include <common.h>
 #include <tcp.h>
@@ -8,6 +9,14 @@
 #include <tnc2.h>
 #include <buffer.h>
 
+static ax25_addr_t own_call;
+
+static int num_untraced_aliases = 0;
+static ax25_addr_t untraced_aliases[16];
+
+static int num_traced_aliases = 0;
+static ax25_addr_t traced_aliases[16];
+
 static uint8_t tnc2_output_buffer[512];
 static volatile sig_atomic_t g_shutdown_requested = 0;
 
@@ -15,6 +24,82 @@ static void signal_handler(int sig)
 {
     (void)sig;
     g_shutdown_requested = 1;
+}
+
+static void configure_untraced_alias(const char *alias, int max_hops)
+{
+    char tmp[16] = {0};
+    for (int hops = 1; hops <= max_hops; hops++)
+    {
+        snprintf(tmp, 16, "%s%d", alias, hops);
+        ax25_addr_init_with(&untraced_aliases[num_untraced_aliases++], tmp, 0, false);
+    }
+}
+
+static void configure_traced_alias(const char *alias, int max_hops)
+{
+    char tmp[16] = {0};
+    for (int hops = 1; hops <= max_hops; hops++)
+    {
+        snprintf(tmp, 16, "%s%d", alias, hops);
+        ax25_addr_init_with(&traced_aliases[num_traced_aliases++], tmp, 0, false);
+    }
+}
+
+static bool base_call_equal(const ax25_addr_t *addr1, const ax25_addr_t *addr2)
+{
+    if (addr1 == NULL)
+        return addr2 == NULL;
+    if (addr2 == NULL)
+        return addr1 == NULL;
+    return !strncasecmp(addr1->callsign, addr2->callsign, AX25_ADDR_MAX_CALLSIGN_LEN);
+}
+
+static void process_packet(ax25_packet_t *packet)
+{
+    buffer_t tnc2_buf = {
+        .data = tnc2_output_buffer,
+        .capacity = sizeof(tnc2_output_buffer),
+        .size = 0};
+
+    if (tnc2_packet_to_string(packet, &tnc2_buf) < 0)
+    {
+        LOG("unable to encode packet to tnc2");
+        return;
+    }
+
+    LOG("rx %s", (char *)tnc2_buf.data);
+
+    int own_call_idx = -1;
+    int untraced_alias_idx = -1;
+    int traced_alias_idx = -1;
+
+    for (int i = 0; i < packet->path_len; i++)
+    {
+        ax25_addr_t *addr = &packet->path[i];
+        if (addr->repeated)
+            continue;
+
+        if (own_call_idx < 0 && base_call_equal(addr, &own_call))
+            own_call_idx = i;
+
+        for (int j = 0; j < num_untraced_aliases && untraced_alias_idx < 0; j++)
+            if (base_call_equal(addr, &untraced_aliases[j]))
+                untraced_alias_idx = i;
+
+        for (int j = 0; j < num_traced_aliases && traced_alias_idx < 0; j++)
+            if (base_call_equal(addr, &traced_aliases[j]))
+                traced_alias_idx = i;
+    }
+
+    if (own_call_idx >= 0)
+        LOG("own call at index %d", own_call_idx)
+
+    if (untraced_alias_idx >= 0)
+        LOG("untraced alias at index %d", untraced_alias_idx)
+
+    if (traced_alias_idx >= 0)
+        LOG("traced alias at index %d", traced_alias_idx)
 }
 
 static void process_kiss(kiss_decoder_t *decoder, uint8_t byte, ax25_packet_t *packet)
@@ -36,18 +121,7 @@ static void process_kiss(kiss_decoder_t *decoder, uint8_t byte, ax25_packet_t *p
         return;
     }
 
-    buffer_t tnc2_buf = {
-        .data = tnc2_output_buffer,
-        .capacity = sizeof(tnc2_output_buffer),
-        .size = 0};
-
-    if (tnc2_packet_to_string(packet, &tnc2_buf) < 0)
-    {
-        LOG("unable to encode packet to tnc2");
-        return;
-    }
-
-    LOG("rx %s", (char *)tnc2_buf.data);
+    process_packet(packet);
 }
 
 int main(void)
@@ -55,7 +129,7 @@ int main(void)
     tcp_client_t client;
     kiss_decoder_t decoder;
     ax25_packet_t packet;
-    
+
     char tcp_buf_data[TCP_READ_BUF_SIZE];
     buffer_t tcp_buf = {
         .data = tcp_buf_data,
@@ -69,6 +143,16 @@ int main(void)
     signal(SIGTERM, signal_handler);
 
     kiss_decoder_init(&decoder);
+    ax25_addr_init_with(&own_call, "SR5DZ", 0, false);
+
+    num_untraced_aliases = 0;
+    configure_untraced_alias("SP", 2);
+    configure_untraced_alias("XR", 2);
+    configure_untraced_alias("ND", 2);
+
+    num_traced_aliases = 0;
+    configure_traced_alias("WIDE", 2);
+    configure_traced_alias("TRACE", 2);
 
     LOG("connecting to TNC at 192.168.0.9:8144...");
 
